b69f8623d4c021fddc73d05411f2ec8f07f59e06
uchida125 <78518033+uchida125@users.noreply.github.com>
2023-06-29 11:49:30 +0900

    コンポーネントの分割

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { User } from './user';
+import { UserListItemComponent } from './user-list-item/user-list-item.component';
 
 @Component({
   selector: 'my-app',
   standalone: true,
-  imports: [CommonModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
+  imports: [CommonModule, UserListItemComponent],
 })
 export class AppComponent implements OnInit {
   private readonly http = inject(HttpClient);
